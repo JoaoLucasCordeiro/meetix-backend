729e8f062c10793985b5142ed397a549781991e5
OcimarSchroederJR <ocimarschroederjr@gmail.com>
2025-09-09 13:30:14 -0300

    feat: adiciona a entidade Lecture que herda atributos de Event

---
+++ src/main/java/com/meetix/meetix_api/entities/Lecture.java
@@ -0,0 +1,21 @@
+package com.meetix.meetix_api.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Lecture extends Event{
+
+    private String speaker;
+    
+    private String topic;
+
+}
