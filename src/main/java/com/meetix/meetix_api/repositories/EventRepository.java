f0b14d98c6df1ea00d1d5887a184116441d89d1a
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-10-08 14:38:11 -0300

    refactor: remove import desnecessário de CrudRepository no EventRepository

--- src/main/java/com/meetix/meetix_api/repositories/EventRepository.java
+++ src/main/java/com/meetix/meetix_api/repositories/EventRepository.java
@@ -2,7 +2,6 @@ package com.meetix.meetix_api.repositories;
 
 import com.meetix.meetix_api.domain.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
