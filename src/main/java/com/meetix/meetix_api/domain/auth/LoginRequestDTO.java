f8fed8b791fa022edad327675d05211d16f274cc
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-10-30 22:28:49 -0300

    refactor: atualiza LoginRequestDTO com validações de Bean Validation

--- src/main/java/com/meetix/meetix_api/domain/auth/LoginRequestDTO.java
+++ src/main/java/com/meetix/meetix_api/domain/auth/LoginRequestDTO.java
@@ -1,7 +1,13 @@
 package com.meetix.meetix_api.domain.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequestDTO(
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
         String email,
+
+        @NotBlank(message = "Senha é obrigatória")
         String password
-) {
-}
+) {}
