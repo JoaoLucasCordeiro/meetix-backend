4f1860d62c7c6395076f1d083bf0f452c9fda66d
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-10-30 22:25:33 -0300

    feat: cria exceção JwtAuthenticationException para erros de autenticação

--- src/main/java/com/meetix/meetix_api/exception/JwtAuthenticationException.java
+++ src/main/java/com/meetix/meetix_api/exception/JwtAuthenticationException.java
@@ -1,22 +1,11 @@
 package com.meetix.meetix_api.exception;
 
 public class JwtAuthenticationException extends RuntimeException {
-
-    // Construtor com mensagem de erro.
-
     public JwtAuthenticationException(String message) {
         super(message);
     }
 
-    // Construtor com mensagem de erro e causa.
-
     public JwtAuthenticationException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    // Construtor apenas com a causa.
-
-    public JwtAuthenticationException(Throwable cause) {
-        super(cause);
     }
 }
