9b3ab75dd4ad53286cd7b22bb12117d389b40f85
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-10-30 22:24:46 -0300

    feat: cria exceção ResourceNotFoundException para recursos não encontrados

---
+++ src/main/java/com/meetix/meetix_api/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.meetix.meetix_api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
