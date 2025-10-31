d47a794942eca98190e504ea63719a907e353c01
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-10-30 22:26:53 -0300

    refactor: refatora UserRequestDTO com validações usando Bean Validation

--- src/main/java/com/meetix/meetix_api/domain/user/UserRequestDTO.java
+++ src/main/java/com/meetix/meetix_api/domain/user/UserRequestDTO.java
@@ -1,12 +1,27 @@
 package com.meetix.meetix_api.domain.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequestDTO(
+        @NotBlank(message = "Nome é obrigatório")
         String firstName,
+
+        @NotBlank(message = "Sobrenome é obrigatório")
         String lastName,
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
         String email,
+
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
         String password,
+
         String instagram,
+
         String university,
+
         String course
-) {
-}
+) {}
