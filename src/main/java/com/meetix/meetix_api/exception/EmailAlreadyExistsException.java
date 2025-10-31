9222861a14dcecfbe98580c7bab7c6bc38ca8a84
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-10-30 22:24:58 -0300

    feat: cria exceção EmailAlreadyExistsException para emails duplicados

---
+++ src/main/java/com/meetix/meetix_api/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.meetix.meetix_api.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("Email já cadastrado: " + email);
+    }
+}
