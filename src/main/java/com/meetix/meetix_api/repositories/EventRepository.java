44200367d517a3e0a375a513e1101bb9b61960cc
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-10-30 22:47:45 -0300

    refactor: adiciona queries personalizadas no EventRepository para eventos futuros e passados

--- src/main/java/com/meetix/meetix_api/repositories/EventRepository.java
+++ src/main/java/com/meetix/meetix_api/repositories/EventRepository.java
@@ -1,11 +1,24 @@
 package com.meetix.meetix_api.repositories;
 
 import com.meetix.meetix_api.domain.event.Event;
+import com.meetix.meetix_api.domain.event.EventType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
+    List<Event> findByOrganizerId(UUID organizerId);
+    
+    List<Event> findByEventType(EventType eventType);
+    
+    @Query("SELECT e FROM Event e WHERE e.startDateTime >= :now ORDER BY e.startDateTime ASC")
+    List<Event> findUpcomingEvents(LocalDateTime now);
+    
+    @Query("SELECT e FROM Event e WHERE e.startDateTime < :now ORDER BY e.startDateTime DESC")
+    List<Event> findPastEvents(LocalDateTime now);
 }
