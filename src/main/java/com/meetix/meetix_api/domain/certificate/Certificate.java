da296a29c0fba665d3875e89e272fb4241ae9e2c
OcimarSchroederJR <ocimarschroederjr@gmail.com>
2025-11-08 14:18:14 -0300

    feat: cria a entidade Certificate para a feature certificate

---
+++ src/main/java/com/meetix/meetix_api/domain/certificate/Certificate.java
@@ -0,0 +1,41 @@
+package com.meetix.meetix_api.domain.certificate;
+
+import com.meetix.meetix_api.domain.event.EventParticipant;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Certificate {
+
+    @Id
+    @GeneratedValue
+    private UUID certificate_id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participant_id", referencedColumnName = "id", nullable = false, unique = true)
+    private EventParticipant participant;
+
+    @Column(nullable = false, unique = true)
+    private UUID validationCode;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime issueDate;
+
+    public Certificate(EventParticipant participant, UUID validationCode) {
+        this.participant = participant;
+        this.validationCode = validationCode;
+    }
+
+}
