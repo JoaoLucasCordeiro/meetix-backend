49516271aaeeaf92f3b75b6b36e30dfd0416afc5
OcimarSchroederJR <ocimarschroederjr@gmail.com>
2025-11-08 14:20:15 -0300

    feat: Adiciona migração V5 para criar a tabela de certificates

---
+++ src/main/resources/db/migration/V5__create-certificate-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE certificates (
+    id UUID PRIMARY KEY,
+    participant_id UUID NOT NULL,
+    validation_code UUID NOT NULL,
+    issue_date TIMESTAMP NOT NULL,
+
+    CONSTRAINT uk_certificate_participant UNIQUE (participant_id),
+    CONSTRAINT uk_certificate_validation_code UNIQUE (validation_code),
+
+    CONSTRAINT fk_certificate_participant
+        FOREIGN KEY (participant_id)
+        REFERENCES event_participant (id)
+        ON DELETE CASCADE
+);
